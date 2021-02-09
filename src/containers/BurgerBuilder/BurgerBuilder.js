2d58f5bbe9122e4585055bab771f91560955fd60
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-02-09 17:04:16 -0300

    Adding the Build Control Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,10 +9,10 @@ class BurgerBuilder extends Component {
     // }
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 3,
+            salad: 2,
+            bacon: 1,
             cheese: 2,
-            meat: 2
+            meat: 3
         }
     }
     render() {
