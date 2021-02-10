268bd3a2e50cc9c5a3939cfad788ae87c421a9f6
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-02-10 10:39:28 -0300

    Adding a Toobar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@
 import classes from './Layout.css';
 import React from 'react';
 import Aux from '../../hoc/Auxi';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <main className="Content">
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import './Toolbar.css';
+import React from 'react';
+
+const toolbar = (props) => (
+    <header className="Toolbar">
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default toolbar;
