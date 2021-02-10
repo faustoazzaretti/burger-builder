ab51d31537691103b4e04b815c0b3de4ceb87186
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-02-10 10:09:28 -0300

    Implementing the Backdrop Component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,22 @@
 import './Modal.css';
 import React from 'react';
+import Aux from '../../../hoc/Auxi';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-    <div
-        className="Modal"
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}
-    >
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        < div
+            className="Modal"
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}
+        >
 
-        {props.children}
-    </div>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,12 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: true })
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false })
+    }
+
+
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -85,7 +91,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}
+                >
                     <OrderSummary
                         ingredients={this.state.ingredients}
                     />
